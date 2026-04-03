0a88da7e865c269c2cbdaed8058439b4aef7d427
Otávio Vailatti Simionato <95048762+Otisimi@users.noreply.github.com>
2026-04-03 17:50:11 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Hoje é dia de Grêmio! Vamo pra cima Grêmiooo fiu fiu!</h1>;
+function Home() {
+  return <h1>Hoje é dia de Grêmio! Vamo pra cima Grêmiooo fiu fiu! 🔵⚫⚪</h1>;
 }
 
 export default Home;
