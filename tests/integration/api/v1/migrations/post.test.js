d7634bc4f436ccb5921d9278bf30c2f9c53830af
Otávio Vailatti Simionato <95048762+Otisimi@users.noreply.github.com>
2026-04-25 22:28:17 +0000

    add `api/v1/migratins`

---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,29 @@
+import database from "infra/database";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public");
+}
+
+test("POST to /api/v1/migrations should return 200", async () => {
+  const responseOne = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(responseOne.status).toBe(201);
+
+  const responseOneBody = await responseOne.json();
+
+  expect(Array.isArray(responseOneBody)).toBe(true);
+  expect(responseOneBody.length).toBeGreaterThan(0);
+
+  const responseTwo = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(responseTwo.status).toBe(200);
+
+  const responseTwoBody = await responseTwo.json();
+
+  expect(Array.isArray(responseTwoBody)).toBe(true);
+  expect(responseTwoBody.length).toBe(0);
+});
