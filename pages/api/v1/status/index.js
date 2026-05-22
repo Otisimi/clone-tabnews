7898424b1c5cbdae96d8a712bbdfa72dd71d0a86
Otávio Vailatti Simionato <95048762+Otisimi@users.noreply.github.com>
2026-05-22 21:41:24 +0000

    feat: create and use `InternalServerError` in `api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,31 +1,45 @@
 import database from "infra/database.js";
+import { InternalServerError } from "infra/errors";
 
 async function status(request, response) {
-  const updatedAt = new Date().toISOString();
+  try {
+    const updatedAt = new Date().toISOString();
 
-  const queryPgVersion = await database.query("SHOW server_version;");
-  const pgVersion = queryPgVersion.rows[0].server_version;
+    const queryPgVersion = await database.query("SHOW server_version;");
+    const pgVersion = queryPgVersion.rows[0].server_version;
 
-  const queryMaxConnections = await database.query("SHOW max_connections;");
-  const maxConnections = parseInt(queryMaxConnections.rows[0].max_connections);
+    const queryMaxConnections = await database.query("SHOW max_connections;");
+    const maxConnections = parseInt(
+      queryMaxConnections.rows[0].max_connections,
+    );
 
-  const dbName = process.env.POSTGRES_DB;
-  const queryActiveConnections = await database.query({
-    text: "SELECT count(*)::int as active_con FROM pg_stat_activity WHERE datname = $1;",
-    values: [dbName],
-  });
-  const activeConnections = queryActiveConnections.rows[0].active_con;
+    const dbName = process.env.POSTGRES_DB;
+    const queryActiveConnections = await database.query({
+      text: "SELECT count(*)::int as active_con FROM pg_stat_activity WHERE datname = $1;",
+      values: [dbName],
+    });
+    const activeConnections = queryActiveConnections.rows[0].active_con;
 
-  response.status(200).json({
-    updated_at: updatedAt,
-    dependencies: {
-      database: {
-        version: pgVersion,
-        max_connections: maxConnections,
-        active_connections: activeConnections,
+    response.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version: pgVersion,
+          max_connections: maxConnections,
+          active_connections: activeConnections,
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    });
+
+    console.log("\n Erro dentro do catch do controler");
+    console.error(publicErrorObject);
+
+    response.status(500).json(publicErrorObject);
+  }
 }
 
 export default status;
