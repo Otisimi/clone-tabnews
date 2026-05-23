508b0fe0f5cce521e0d19046c0dea7b1854a4f81
Otávio Vailatti Simionato <95048762+Otisimi@users.noreply.github.com>
2026-05-23 18:34:07 +0000

    refactor: use `next connect` in `/migrations` and `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,45 +1,37 @@
+import { createRouter } from "next-connect";
 import database from "infra/database.js";
-import { InternalServerError } from "infra/errors";
+import controller from "infra/controller";
 
-async function status(request, response) {
-  try {
-    const updatedAt = new Date().toISOString();
+const router = createRouter();
 
-    const queryPgVersion = await database.query("SHOW server_version;");
-    const pgVersion = queryPgVersion.rows[0].server_version;
+router.get(getHandler);
 
-    const queryMaxConnections = await database.query("SHOW max_connections;");
-    const maxConnections = parseInt(
-      queryMaxConnections.rows[0].max_connections,
-    );
+export default router.handler(controller.errorHandlers);
 
-    const dbName = process.env.POSTGRES_DB;
-    const queryActiveConnections = await database.query({
-      text: "SELECT count(*)::int as active_con FROM pg_stat_activity WHERE datname = $1;",
-      values: [dbName],
-    });
-    const activeConnections = queryActiveConnections.rows[0].active_con;
+async function getHandler(request, response) {
+  const updatedAt = new Date().toISOString();
 
-    response.status(200).json({
-      updated_at: updatedAt,
-      dependencies: {
-        database: {
-          version: pgVersion,
-          max_connections: maxConnections,
-          active_connections: activeConnections,
-        },
+  const queryPgVersion = await database.query("SHOW server_version;");
+  const pgVersion = queryPgVersion.rows[0].server_version;
+
+  const queryMaxConnections = await database.query("SHOW max_connections;");
+  const maxConnections = parseInt(queryMaxConnections.rows[0].max_connections);
+
+  const dbName = process.env.POSTGRES_DB;
+  const queryActiveConnections = await database.query({
+    text: "SELECT count(*)::int as active_con FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+  const activeConnections = queryActiveConnections.rows[0].active_con;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: pgVersion,
+        max_connections: maxConnections,
+        active_connections: activeConnections,
       },
-    });
-  } catch (error) {
-    const publicErrorObject = new InternalServerError({
-      cause: error,
-    });
-
-    console.log("\n Erro dentro do catch do controler");
-    console.error(publicErrorObject);
-
-    response.status(500).json(publicErrorObject);
-  }
+    },
+  });
 }
-
-export default status;
